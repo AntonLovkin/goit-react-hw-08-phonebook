22d877ab054efbdf3431b5c774d519c8850b4f54
AntonLovkin <anton28111987@gmail.com>
2021-07-25 22:27:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import AppBar from './UserMenu/AppBar';
-import ContactsView from './views/ContactsView';
-import RegisterView from './views/RegisterView';
-import LoginView from './views/LoginView';
-import { Component } from 'react';
-import { render } from '@testing-library/react';
+// import ContactsView from './views/ContactsView';
+// import RegisterView from './views/RegisterView';
+// import LoginView from './views/LoginView';
+import { Component, lazy, Suspense } from 'react';
 import authOperations from './redux/auth/auth-operations';
 import { connect } from 'react-redux';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+
+const ContactsView = lazy(() => import('./views/ContactsView'));
+const RegisterView = lazy(() => import('./views/RegisterView'));
+const LoginView = lazy(() => import('./views/LoginView'));
 
 class App extends Component {
 
@@ -18,11 +23,14 @@ class App extends Component {
     return (
       <>
         <AppBar />
-        <Switch>
-          <Route path="/register" component={RegisterView} />
-          <Route path="/login" component={LoginView} />
-          <Route path="/contacts" component={ContactsView} />
-        </Switch>
+
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <PublicRoute path="/register" restricted component={RegisterView} redirectTo='/contacts'/>
+            <PublicRoute path="/login" restricted component={LoginView} redirectTo='/contacts'/>
+            <PrivateRoute path="/contacts" component={ContactsView} redirectTo='/login' />
+          </Switch>
+        </Suspense>
       </>
     );
   }
