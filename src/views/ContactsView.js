23899e2318b931b87fb2d76359d0e97310cbe178
AntonLovkin <anton28111987@gmail.com>
2021-07-25 23:16:32 +0300

    Update ContactsView.js

--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import ContactForm from '../components/Contact-form';
 import Filter from '../components/Filter';
 import ContactsList from '../components/Contacts';
-// import Container from '../components/Container';
-// import TodoList from '../components/TodoList';
-// import TodoEditor from '../components/TodoEditor';
-// import Filter from '../components/TodoFilter';
-// import Stats from '../components/Stats';
-// import Modal from '../components/Modal';
-// import IconButton from '../components/IconButton';
-// import { ReactComponent as AddIcon } from '../icons/add.svg';
-// import { todosOperations, todosSelectors } from '../redux/todos';
 
-const barStyles = {
-  display: 'flex',
-  alignItems: 'flex-end',
-  marginBottom: 20,
-};
+const ContactsView = () => {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm />
 
-class ContactsView extends Component {
-  state = {
-    showModal: true,
-  };
+        <h2>Filter</h2>
+        <Filter />
+
+        <h2>Contacts</h2>
+        <ContactsList />
+      </>
+    );
+  }
+
+
+export default ContactsView;
+
+
+//------------------------------------------------
+// const barStyles = {
+//   display: 'flex',
+//   alignItems: 'flex-end',
+//   marginBottom: 20,
+// };
+
+// class ContactsView extends Component {
+  // state = {
+  //   showModal: true,
+  // };
 
   // componentDidMount() {
   //   this.props.fetchTodos();
@@ -34,41 +43,41 @@ class ContactsView extends Component {
   //   }));
   // };
 
-  render() {
-    // const { showModal } = this.state;
+//   render() {
+//     // const { showModal } = this.state;
 
-    return (
-      <>
-        {/* <div style={barStyles}> */}
-          {/* <Filter />
-          <Stats /> */}
-          {/* <IconButton onClick={this.toggleModal} aria-label="Добавить todo">
-            <AddIcon width="40" height="40" fill="#fff" />
-          </IconButton> */}
+//     return (
+//       <>
+//         {/* <div style={barStyles}> */}
+//           {/* <Filter />
+//           <Stats /> */}
+//           {/* <IconButton onClick={this.toggleModal} aria-label="Добавить todo">
+//             <AddIcon width="40" height="40" fill="#fff" />
+//           </IconButton> */}
 
-          {/* {this.props.isLoadingTodos && <h1>Загружаем...</h1>}
-        </div> */}
+//           {/* {this.props.isLoadingTodos && <h1>Загружаем...</h1>}
+//         </div> */}
 
-        {/* <TodoList /> */}
+//         {/* <TodoList /> */}
 
-        <h1>Phonebook</h1>
-        <ContactForm />
+//         <h1>Phonebook</h1>
+//         <ContactForm />
 
-        <h2>Filter</h2>
-        <Filter />
+//         <h2>Filter</h2>
+//         <Filter />
 
-        <h2>Contacts</h2>
-        <ContactsList />
+//         <h2>Contacts</h2>
+//         <ContactsList />
 
-        {/* {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <TodoEditor onSave={this.toggleModal} />
-          </Modal>
-        )} */}
-      </>
-    );
-  }
-}
+//         {/* {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//             <TodoEditor onSave={this.toggleModal} />
+//           </Modal>
+//         )} */}
+//       </>
+//     );
+//   }
+// }
 
 // const mapStateToProps = state => ({
 //   isLoadingTodos: todosSelectors.getLoading(state),
@@ -79,4 +88,4 @@ class ContactsView extends Component {
 // });
 
 // export default connect(mapStateToProps, mapDispatchToProps)(ContactsView);
-export default ContactsView;
+// export default ContactsView;
